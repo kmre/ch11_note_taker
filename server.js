62b5f2b4a6cfa11a49ff0b73f4c4acb35ebd8876
kmre <karla.romero7@gmail.com>
2021-08-07 02:16:31 -0400

    finally got it to work

--- server.js
+++ server.js
@@ -7,32 +7,16 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const path = require("path");
 const fs = require("fs");
+//Generate Unique ID
+const generateUniqueId = require('generate-unique-id');
 
 var arrayNotes = [];
 var indexPath = path.join(__dirname, "./public/index.html");
-var notePath = path.join(__dirname, "./public/notes.html");
-
-
-//const {notes} = require("./miniature-eureka/Develop/db/db");
+var notesPath = path.join(__dirname, "./public/notes.html");
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
-
-
-//Generate Unique ID
-const generateUniqueId = require("generate-unique-id");
-
-//Create New Note
-function createNewNote(body, arrayNotes) {
-    const note = body;
-    arrayNotes.push(note);
-    fs.writeFileSync(
-      path.join(__dirname, './miniature-eureka/Develop/db/db.json'),
-      JSON.stringify({ notes: arrayNotes}, null, 2)
-    );
-    return note;
-  };  
 
 //Routes
 app.get('/', (req, res) => {
@@ -40,30 +24,39 @@ app.get('/', (req, res) => {
 });
 
 app.get('/notes', (req, res) => {
-  res.sendFile(notePath);
+  res.sendFile(notesPath);
 });
 
-// app.get('/api/notes', (req, res) => {
-//   res.json(notes);
-// });
+app.get('/api/notes', (req, res) => {
+res.json(arrayNotes);
+});
 
-// app.post('/api/notes', (req, res) => {
-//   req.body.id = generateUniqueId();
-//   const note = createNewNote(req.body, notes);
-//   res.json(note);
-// });
+app.post('/api/notes', (req, res) => {
+  const newNote = req.body;
+  //newNote.id = arrayNotes.length.toString();
+  newNote.id = generateUniqueId();
+  arrayNotes.push(newNote);
+  console.log(arrayNotes)
+  fs.writeFile('./db/db.json', JSON.stringify(arrayNotes),(err) => {
+    if (err) {
+        console.log("Error occurred: " + err)
+    }
+  })
+  res.json(arrayNotes);
+});
     
 //Bonus - DELETE 
 //As noted above this code reference was found here: https://www.tabnine.com/code/javascript/functions/express/Express/delete
 //Source originally found by Emily
-// app.delete('/api/notes/:id', (req, res) => {
-//   const { id } = req.params;
-
-//   const delNote = notes.findIndex(note => note.id ==id);
-
-//   notes.splice(delNote, 1);
-//   return res.send();
-// });
+app.delete('/api/notes/:id', (req, res) => {
+  const {id} = req.params;
+  console.log("ID: " + id)
+  const deleteNote = arrayNotes.findIndex(body => body.id == id)
+  console.log("Delete Note: " + deleteNote)
+  arrayNotes.splice(deleteNote, 1);
+  console.log(arrayNotes);
+  return res.send();
+});
 
 //listen for port
 app.listen(PORT, () => {
